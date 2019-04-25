1a636f1d5992c3d8aacd9a0430231f0b7295e33c
Renato Cavalcanti <renato@cavalcanti.be>
2019-04-25 11:02:48 +0200

    add gh functions and auto-completion

---
+++ gh-functions.sh
@@ -0,0 +1,163 @@
+function gh.org {
+  # if we got a project folder, we just use it. 
+  if [ $2 ] && [ -d $GITHUB_SRC/$1/$2 ]; then
+    # drop ORG and PROJ and pass the rest
+    PROJ_MASTER=$1/$2; shift; shift
+    gh $PROJ_MASTER $@ # $@ minus $1 and $2
+  else
+    # $1 + $2 don't make a project, try org/master
+    ORG=$1; shift
+    PROJ_MASTER=$ORG/$ORG/master
+    if [[ -d $GITHUB_SRC/$PROJ_MASTER ]]; then
+      gh $PROJ_MASTER $@ # $@ minus $1
+    else 
+      gh $ORG $@ # $@ minus $1
+    fi
+  fi
+
+}
+
+function gh() {
+
+  function usage.gh() {
+    echo "TODO: usage"
+  }
+  
+  if [ $1 ] && [ -d $GITHUB_SRC/$1 ]; then
+    
+    DIR=$1; shift
+    PROJ_DIR=$GITHUB_SRC/$DIR
+
+    if [[ $# -eq 0 ]]; then
+      ORG=`echo $DIR | cut -d "/" -f 1`
+      ORG_DIR=$GITHUB_SRC/$ORG
+      if [ -f $ORG_DIR/.zshrc ]; then
+        source $ORG_DIR/.zshrc
+      fi
+      echo "switching to $PROJ_DIR"
+      cd $PROJ_DIR
+    else
+   
+      while [[ $# -gt 0 ]]
+      do
+          case $1 in
+
+              -e|--edit)
+                  shift
+                  (
+                    cd $PROJ_DIR
+                    edit .
+                  )
+                  ;;
+
+              -i|--intellij)
+                  shift
+                  (
+                    cd $PROJ_DIR
+                    ij .
+                  )
+                  ;;
+
+              -b|--branch)
+                  shift
+                  BRANCH_NAME=$1; shift
+                  cd $PROJ_DIR
+                  workbranch $BRANCH_NAME
+                  ;;
+
+              -pco|--pr-checkout)
+                  shift
+                  PR_NUMBER=$1; shift
+                  cd $PROJ_DIR
+                  pr.checkout $PR_NUMBER
+                  ;;
+
+
+              -s|--status)
+                  shift
+                  (
+                    echo "Status for $DIR"
+                    cd $PROJ_DIR
+                    pr.status
+                  )
+                  ;;
+
+              -o|--open)
+                  shift
+                  (
+                    echo "Browse $DIR"
+                    cd $PROJ_DIR
+                    pr.open
+                  )
+                  ;;
+
+              -d|--delete)
+                  shift
+                  (
+                    cd $PROJ_DIR
+                    git.branch.delete .
+                  )
+                  ;;
+
+              -D|--force-delete)
+                  shift
+                  (
+                    cd $PROJ_DIR
+                    git.branch.delete.force .
+                  )
+                  ;;
+
+              -l|--pr-list)
+                  shift
+                  (
+                    cd $PROJ_DIR
+                    hub pr list $@
+                  )
+                  ;;
+              
+              -g|--go-to)
+                  shift
+                  cd $PROJ_DIR
+                  ;;
+
+              -h|--help)
+                  shift
+                  usage.gh
+                  ;;
+                  
+          esac
+      done
+
+    fi
+  fi
+}
+
+
+# same as _gh_completions but starting at org level
+_gh_completions_org ()   {       
+  
+  SRC_ROOT=$GITHUB_SRC/$1
+  #            list all projecst    | cut out root         | cut leading chars  | cut out trailing /
+  GH_PROJECTS=`ls -d $SRC_ROOT/*/*/ | cut -c ${#SRC_ROOT}- | cut -c 3-          | sed 's/\/$//'`
+  
+  local cur
+  COMPREPLY=()     
+  cur=${COMP_WORDS[COMP_CWORD]}
+  COMPREPLY=( $(compgen -W "${GH_PROJECTS}" -- ${cur}) )
+
+  return 0
+}
+
+_gh_completions ()   {       
+  #            list all projecst        | cut out root           | cut leading chars  | cut out trailing /
+  GH_PROJECTS=`ls -d $GITHUB_SRC/*/*/*/ | cut -c ${#GITHUB_SRC}- | cut -c 3-          | sed 's/\/$//'`
+
+  local cur
+  COMPREPLY=()     
+  cur=${COMP_WORDS[COMP_CWORD]}
+  COMPREPLY=( $(compgen -W "${GH_PROJECTS}" -- ${cur}) )
+
+  return 0
+}
+
+complete -F _gh_completions -o plusdirs gh 
---
+++ git-functions.sh
@@ -0,0 +1,168 @@
+
+function git.rebase.interative {
+    if [ $1 ] ; then
+        git rebase -i HEAD~"$1"
+    else
+        echo "A HEAD~{num} must be provided"
+    fi
+}
+
+
+# create a working branch, similar to `git worktree`
+# but works by creating a local clone 
+
+function git.workbranch {
+    if [ -d .git ]; then
+      if [ $1 ]; then 
+          git-pull-upstream
+          
+          REPO=${PWD##*/}
+
+          # save remote urls for later
+          UPSTREAM_REMOTE_URL=`git remote get-url upstream`
+          ORIGIN_REMOTE_URL=`git remote get-url origin`
+
+          cd ../
+          git clone ${REPO} $1
+          cd $1
+
+          # local points to origin (original local checkout)
+          git remote add local `git remote get-url origin`
+
+          # remove origin and read point to remove
+          git remote remove origin
+          git remote add origin $ORIGIN_REMOTE_URL
+
+          # add upstream pointing to remote
+          git remote add upstream $UPSTREAM_REMOTE_URL
+
+          # check it out using new branch
+          git checkout -b $1
+
+          # copy ide settings
+          copy-ide-settings ${REPO} $1          
+          cd ../$1
+        else 
+          echo "branch name must be passed, usage: git wbranch some-name"
+        fi
+    else
+      echo "Not a git repository"
+    fi
+
+}
+
+
+function _pr.url() {
+  hub pr list -s all -f %U -h $GITHUB_USER:$(git rev-parse --abbrev-ref HEAD)
+}
+
+function _pr.api.url() {
+  PR_URL=$1
+  PATH_URL=`echo ${PR_URL#*https://github.com/} | sed  's/\/pull\//\/pulls\//g'`
+  echo "https://api.github.com/repos/$PATH_URL" 
+}
+
+function pr.open {
+  if [ $1 ]; then
+    cd $1
+  fi
+
+  PR_URL=`_pr.url`
+  if [ $PR_URL ]; then
+    open $PR_URL   
+  fi
+}
+
+function pr.status {
+  if [ $1 ]; then
+    cd $1
+  fi 
+
+  PR_URL=`_pr.url`
+  if [ $PR_URL ]; then
+      PR_API_URL=`_pr.api.url $PR_URL`
+      # call rest api
+      JSON=`curl -s $PR_API_URL |  jq '{state, merged}'`
+      MERGED=`echo $JSON | jq '.merged'`
+      STATE=`echo $JSON | jq '.state'`
+      CI_STATUS=`hub ci-status`
+
+      echo
+      echo "URL: $PR_URL" 
+      echo "Merged: $MERGED" 
+      echo "State: ${STATE//\"}" 
+      echo "CI Stauts: $CI_STATUS"
+  else
+      DIR=`pwd`
+      echo "No found for branch in $DIR"
+  fi
+}
+
+
+function pr.merged {
+  if [ $1 ]; then
+    cd $1
+  fi
+
+  PR_URL=`_pr.url`
+  if [ $PR_URL ]; then
+      PR_API_URL=`_pr.api.url $PR_URL`
+      # call rest api
+      curl -s $PR_API_URL | jq '.merged'
+  else
+    echo "no PR found"
+  fi
+}
+
+function pr.new {
+  git-push-origin
+  PR_URL=$(hub pull-request $@)
+  PR_API_URL=`_pr.api.url $PR_URL`
+
+  echo "PR URL: $PR_URL"
+  echo "PR API URL: $PR_API_URL"
+}
+
+function gc.pr {
+  git add .
+  git commit -m $1
+  pr.new -m $1
+}
+
+function git.branch.delete.force {
+    if [ -d .git ]; then
+      echo "Deleting branch '$(git-current-branch)' on origin"
+      git push --delete origin `git-current-branch`
+
+      # delete that folder
+      DIR_TO_DELETE=`pwd`
+
+      echo "Deleting dir '$DIR_TO_DELETE'"
+      rm -rf $DIR_TO_DELETE
+    else 
+      echo "Not a git repository"
+    fi 
+  }
+
+function git.branch.delete {
+  PR_STATE=`pr.merged` 
+  if [ "$PR_STATE" = "true" ]; then
+      git.branch.delete.force $1
+  else 
+      PR_URL=`_pr.url` 
+      echo "This branch $i has an open and unmerged PR"
+      echo "Check it at $PR_URL"
+      echo "To force a delete, call git.branch.delete.force instead"
+  fi 
+}
+
+alias pr.list='hub pr list'
+
+
+function pr.checkout {
+  git.workbranch "PR-$1"
+  hub pr checkout $1
+}
+
+alias backport='git backport'
+alias workbranch='git.workbranch'
